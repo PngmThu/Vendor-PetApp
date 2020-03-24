652f101130f361dc3ff0745eed3dc83fb4110b34
cuongquangnam <tccuong1999@gmail.com>
2020-03-25 02:32:32 +0800

    done ScheduleModel

---
+++ models/ScheduleModel.js
@@ -0,0 +1,17 @@
+import BaseModel from './BaseModel';
+
+export class Schedule extends BaseModel{
+
+    constructor(obj){
+        super(obj, Schedule.schema)
+    }
+
+    static schema = {
+        date: {type: 'object'},
+        vendorId: {type: 'string'}
+    }
+
+    resolveData(){
+        return super.resolveData(this);
+    }
+}
