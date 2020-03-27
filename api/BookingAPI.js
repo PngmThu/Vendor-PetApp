b3a2b65d2200b0126577aa47d8ae482b31d8b782
weilun <48602497+noobsonli@users.noreply.github.com>
2020-03-27 17:43:21 +0800

    d

--- api/BookingAPI.js
+++ api/BookingAPI.js
@@ -6,16 +6,80 @@ export default class BookingAPI{
         this.globals = new Globals();
     }
 
-    getBookingList(){
+    postBooking(token,petId,serviceId,vendorId,customerId){
 
-        const url = this.globals.serverHost + '/api/booking';
-        let token = "To be added later";
+        const url = this.globals.serverHost + '/api/booking/add';
 
         let options = {
             headers: {token: token, 'Access-Control-Allow-Origin':'*'}
         };
 
-        let body = {};
-        return axios.get(url, body, options)
+        let body = {petId: petId, serviceId : serviceId, vendorId: vendorId, customerId:customerId};
+
+        return axios.post(url, body, options);
     }
+    
+    getBookingById(token, id){
+        const url = this.globals.serverHost + '/api/booking/' + id;
+        
+        let options = {
+            headers: {token: token, 'Access-Control-Allow-Origin':'*'}
+        }
+
+        return axios.get(url,options);
+    }
+
+    getBookingByVendorId(token, vendorId){
+        const url = this.globals.serverHost + '/api/booking/vendor/' + vendorId;
+        
+        let options = {
+            headers: {token: token, 'Access-Control-Allow-Origin':'*'}
+        }
+
+        return axios.get(url,options);
+    }
+
+    getBookingByCustomerId(token, customerId){
+        const url = this.globals.serverHost + '/api/booking/customer/' + customerId;
+        
+        let options = {
+            headers: {token: token, 'Access-Control-Allow-Origin':'*'}
+        }
+
+        return axios.get(url,options);
+    }
+
+    getBookingByPetId(token, PetId){
+        const url = this.globals.serverHost + '/api/booking/pet/' + PetId;
+        
+        let options = {
+            headers: {token: token, 'Access-Control-Allow-Origin':'*'}
+        }
+
+        return axios.get(url,options);
+    }
+
+    deleteBookingById(token, bookingId){
+        const url = this.globals.serverHost + '/api/'+ bookingId;
+
+        let options = {
+            headers: {token: token, 'Access-Control-Allow-Origin':'*'}
+        }
+
+        return axios.delete(url,options);
+    }
+
+    updateBookingById(token, bookingId, bookingStatus){
+        const url = this.globals.serverHost + '/api/'+ bookingId;
+
+        let options = {
+            headers: {token: token, 'Access-Control-Allow-Origin':'*'}
+        }
+
+        let body = {status: bookingStatus}
+
+        return axios.post(url, body, options)
+
+    }
+
 }
