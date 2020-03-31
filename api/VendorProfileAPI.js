d15676c114c5bbc2782365c6266a41c57b99a302
Zaphyr Ng <weehau1996@hotmail.com>
2020-03-31 18:11:09 +0800

    Display Profile

--- api/VendorProfileAPI.js
+++ api/VendorProfileAPI.js
@@ -5,10 +5,11 @@ import AuthAPI from './AuthAPI';
 export default class VendorProfileAPI{
     constructor() {
         this.globals = new Globals();
+        this.authAPI = new AuthAPI();
     }
 
-    updateUserById(vendor, vendorId){
-        const token = AuthAPI.retrieveToken();
+    async updateUserById(vendor, vendorId, callback){
+        const token = await this.authAPI.retrieveToken();
 
         const url = this.globals.serverHost + '/api/vendor/' + vendorId;
 
@@ -16,9 +17,18 @@ export default class VendorProfileAPI{
             headers: {token: token, 'Access-Control-Allow-Origin':'*'}
         };
 
-        let body = {vendor};
-        return axios.post(url, body, options)
-
+        axios.put(url, vendor, options)
+        .then(res => {
+            if(res.status == 200){
+                callback(true);
+            }
+            else{
+                callback(false);
+            }
+        })
+        .catch(err => {
+            console.log(err.response.data)
+        })
     }
 
     updatePasswordById(vendor, vendorId){
@@ -35,15 +45,23 @@ export default class VendorProfileAPI{
 
     }
 
-    getUserById(customerId){
-        const token = AuthAPI.retrieveToken();
+    async getUserById(customerId, callback){
+        const token = await this.authAPI.retrieveToken();
 
-        const url = this.globals.serverHost + 'api/vendor/'+ customerId;
+        const url = this.globals.serverHost + '/api/vendor/'+ customerId;
 
         let options = {
             headers: {token: token, 'Access-Control-Allow-Origin':'*'}
         };
 
-        return axios.get(url, options)
+        axios.get(url,options)
+        .then(res => {
+            if(res.status == 200){
+                callback(res.data);
+            }
+        })
+        .catch(err => {
+            console.log(err.response.data);
+        })
     }
 }
--- globals/globals.js
+++ globals/globals.js
@@ -1,4 +1,4 @@
 export default class Globals {
     //serverHost = 'http://10.27.179.156:4000';
-    serverHost = 'https://pure-dawn-15417.herokuapp.com';
+    serverHost = 'https://cz2006.herokuapp.com';
 }
