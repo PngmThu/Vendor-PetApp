8413d83ba17137fcc5a3ca6793d6575a0ba8d91a
Zaphyr Ng <weehau1996@hotmail.com>
2020-03-31 18:34:26 +0800

    William

---
+++ globals/globals.js
@@ -0,0 +1,5 @@
+export default class Globals {
+    //serverHost = 'http://10.27.179.156:4000';
+    serverHost = 'https://pure-dawn-15417.herokuapp.com';
+    //serverHost = 'http://192.168.56.1:4000'
+} 
