343d668b30f226391ec041f57946bd5752806750
khanh <khanh.phungntu@gmail.com>
2020-03-31 23:51:27 +0800

    noti

--- globals/globals.js
+++ globals/globals.js
@@ -1,4 +1,4 @@
 export default class Globals {
-    serverHost = 'http://192.168.0.101:4000';
-    //serverHost = 'https://cz2006.herokuapp.com';
+    //serverHost = 'http://192.168.0.101:4000';
+    serverHost = 'https://cz2006.herokuapp.com';
 }
