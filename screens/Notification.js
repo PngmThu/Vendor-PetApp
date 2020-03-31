b74871a109f3826c620c4eb94dfa366cf79d137f
cuongquangnam <tccuong1999@gmail.com>
2020-03-31 22:12:23 +0800

    make Notification beautiful

--- screens/Notification.js
+++ screens/Notification.js
@@ -23,7 +23,7 @@ const { width, height } = Dimensions.get("screen");
 class Notification extends React.Component {
   constructor(){
     super()
-  this.state = {address: "", notifications: [<Text>"sfh"</Text>]}
+  this.state = {address: "", notifications: []}
   this.authAPI = new AuthAPI();
   this.petAPI = new PetAPI();
   this.customerAPI = new CustomerAPI();
@@ -53,7 +53,7 @@ async componentDidMount(){
                         notif.time = new Date(notif.time);
                         result.push(<View style={styles.agenda}>
                         <View style={styles.leftDetail}>
-                          <View style={{ flexDirection: 'row', backgroundColor: "purple", marginTop: 5, marginLeft:10, width: 40, height: 40, borderRadius: 20 }}>
+                          <View style={{ flexDirection: 'row', backgroundColor: "purple", marginTop: 20, marginLeft:40, width: 40, height: 40, borderRadius: 20 }}>
                             <Icon
                               size={30}
                               color={'#ffffff'}
@@ -62,7 +62,7 @@ async componentDidMount(){
                               style={styles.inputIcons}
                             />
                           </View>
-                          <Text style={{fontWeight:'100', fontSize: 17.5, color: '#ffffff', paddingTop: 0, paddingLeft: 10, paddingRight: 10}}>
+                          <Text style={{fontWeight:'100', fontSize: 17.5, color: '#ffffff', paddingTop: 12, paddingLeft: 10, paddingRight: 12}}>
                           Your booking with customer {customer.firstName} {customer.lastName} on {notif.time.getDate()+1} - 
                           {notif.time.getMonth()+1} - {notif.time.getFullYear()} is {notif. bookingStatus}       </Text>
                         </View>
@@ -127,6 +127,7 @@ async componentDidMount(){
 }
 
 
+
 const styles = StyleSheet.create({
   home: {
     width: width,    
@@ -140,52 +141,8 @@ const styles = StyleSheet.create({
     position: 'absolute'
   },
   inputIcons: {
-    marginRight: 12,
-  },
-  logoutIcon:{
-    color: 'red',
-    fontWeight: '200',
-  },
-  action: {
-    width:"90%",
-    alignSelf: 'center',
-    marginTop: 10
-  },
-  logoutTxt: {
-    color: 'red',
-    marginLeft: 5
-  },
-  editIcon: {
-    color: 'white',
-    fontWeight: '200',
-    textAlign: 'left'
-  },
-  editTxt: {
-    color: 'white',
     marginLeft: 5,
-    textAlign: 'left'
-  },
-  profileButton: {
-    width: width * 0.4,
-    marginTop: 25,
-    borderRadius: 10,
-  },
-  calendar: {
-    width: "95%",
-    height: "100%",
-    borderRadius: 15,
-    elevation: 2,
-    justifyContent: 'center'
-  },
-  circle: {
-    width: 16,
-    height: 16,
-    borderRadius: 8,
-    elevation: 2
-  },
-  annotation: {
-    marginTop: 10,
-    marginBottom:0
+    marginTop: 5,
   },
   agenda: {
     width: "92%",
@@ -195,44 +152,14 @@ const styles = StyleSheet.create({
   leftDetail: {
     width: "100%",
     borderRadius: 12,
-    alignSelf: 'flex-end',
     backgroundColor: 'rgba(45, 45, 45, 0.8)',
     height: 80,
     paddingLeft: 20,
     paddingRight: 20,
     justifyContent: "center",
-    marginBottom: 10
+    marginBottom: 10,
+    flexDirection: 'row'
   },
-  service: {
-    paddingRight: 100,
-    fontSize: 16,
-    fontWeight: "300",
-    color: "white"
-  },
-  time: {
-    paddingRight: 100,
-    fontSize: 17,
-    fontWeight: "600",
-    color: "white"
-  },
-  dayBackground: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    elevation: 2,
-    backgroundColor: "#FF9B70",
-    alignSelf: "flex-start",
-    position: 'absolute',
-    justifyContent: 'center',
-    marginLeft: 10,
-    marginTop: 5
-  },
-  day: {
-    fontSize: 30,
-    fontWeight: "600",
-    textAlign: "center",
-    color: "white"
-  }
 });
 
 export default Notification;
