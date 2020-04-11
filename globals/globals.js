6e3ff1668f412fb2192f06c7d538ff1434767e1d
PngmThu <phamthu0602@gmail.com>
2020-04-12 00:08:22 +0800

    change global

--- globals/globals.js
+++ globals/globals.js
@@ -1,4 +1,4 @@
 export default class Globals {
-    serverHost = 'http://192.168.0.102:4000';
-    //serverHost = 'https://cz2006.herokuapp.com';
+    //serverHost = 'http://192.168.0.102:4000';
+    serverHost = 'https://cz2006.herokuapp.com';
 }
